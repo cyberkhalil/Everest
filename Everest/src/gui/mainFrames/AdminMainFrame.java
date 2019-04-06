c384a5973d291d600adbc86f726c15a3b460dffa
khalil2535 <khalil2535@protonmail.com>
2019-04-06 11:03:20 +0300

    Adjust libraries
    Signed-off-by: khalil2535 <khalil2535@protonmail.com>

--- Everest/src/gui/mainFrames/AdminMainFrame.java
+++ Everest/src/gui/mainFrames/AdminMainFrame.java
@@ -128,7 +128,7 @@ public class AdminMainFrame extends JFrame {
 
         jPanel4.setBackground(new java.awt.Color(255, 255, 255));
         jPanel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(255, 171, 112)));
-        jPanel4.setToolTipText("student");
+        jPanel4.setToolTipText("");
         jPanel4.setPreferredSize(new java.awt.Dimension(573, 450));
 
         jPanel5.setBackground(new java.awt.Color(255, 255, 255));
@@ -247,6 +247,7 @@ public class AdminMainFrame extends JFrame {
         jLabel4.setForeground(new java.awt.Color(0, 51, 153));
         jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("Main Page");
+        jLabel4.setToolTipText("");
         jLabel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 51, 153)));
 
         jPanel9.setBackground(new java.awt.Color(255, 255, 255));
@@ -367,9 +368,9 @@ public class AdminMainFrame extends JFrame {
                     .addComponent(jPanel14, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel6, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
+                .addGap(60, 60, 60)
                 .addComponent(jPanel9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(52, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -383,11 +384,10 @@ public class AdminMainFrame extends JFrame {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, 0)
-                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
-
-        jPanel4.getAccessibleContext().setAccessibleDescription("");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -417,6 +417,19 @@ public class AdminMainFrame extends JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void edit_examsjButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edit_examsjButtonActionPerformed
+        JFrame frame = new EditExamsFrame();
+        frame.setVisible(true);
+        edit_examsjButton.setEnabled(false);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                edit_examsjButton.setEnabled(true);
+            }
+        });
+
+    }//GEN-LAST:event_edit_examsjButtonActionPerformed
 
     private void new_examjButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_new_examjButtonActionPerformed
         new ExamAddFrame(new_examjButton).setVisible(true);
@@ -474,20 +487,6 @@ public class AdminMainFrame extends JFrame {
         root.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_LogOutBtnActionPerformed
-
-    private void edit_examsjButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edit_examsjButtonActionPerformed
-        JFrame frame = new EditExamsFrame();
-        frame.setVisible(true);
-        edit_examsjButton.setEnabled(false);
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                edit_examsjButton.setEnabled(true);
-            }
-        });
-
-
-    }//GEN-LAST:event_edit_examsjButtonActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton AddUserBtn;
